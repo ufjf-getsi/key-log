93dc738c0dbd28be521a29fbc7dc0d347a0eee7f
Igor Knop <igorknop@gmail.com>
2019-04-04 17:07:29 -0300

    Corrige nome e remote dados de cofigiuração

---
+++ src/firebaseConfig-dist.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+};
+//Pegue os dados no console do firebase
--- src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyBX_gKAF-w76A89ROXxtDS5nS8lGsEM9tA",
-    authDomain: "key-log-79327.firebaseapp.com",
-    databaseURL: "https://key-log-79327.firebaseio.com",
-    projectId: "key-log-79327",
-    storageBucket: "key-log-79327.appspot.com",
-    messagingSenderId: "532043359773"
-};
