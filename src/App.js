c387b83835c7fecbb7216e6d1ed51d8d6016c0e6
josueodh <josuedelgadoheringer98@gmail.com>
2019-04-05 15:48:55 -0300

    Formulario

--- src/App.js
+++ src/App.js
@@ -1,49 +1,54 @@
 import React, { Component } from 'react';
-import withFirebaseAuth from 'react-with-firebase-auth'
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import firebaseConfig from './firebaseConfig';
 import logo from './logo.svg';
 import './App.css';
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
 class App extends Component {
+  constructor(){
+    super();
+    this.state = { nome: "Fulano", pessoas:[]};
+  }
+  noClick(){
+    this.state.pessoas.push(this.state.nome);
+
+    this.setState({ nome: "", pessoas: this.state.pessoas });
+    console.log(this.state.pessoas);
+    
+  }
+
   render() {
-    const {
-      user,
-      signOut,
-      signInWithGoogle,
-    } = this.props;
+    let liPessoas = [];
+    for (let index = 0; index < this.state.pessoas.length; index++) {
+      const element = this.state.pessoas[index];
+      liPessoas.push(<li key={index+element}>{element}</li>)
+    }
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {
-            user
-              ? <p>Hello, {user.displayName}</p>
-              : <p>Please sign in.</p>
-          }
-
-          {
-            user
-              ? <button onClick={signOut}>Sign out</button>
-              : <button onClick={signInWithGoogle}>Sign in with Google</button>
-          }
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          
+          </a>
+          <input value={this.state.nome} onChange={(e)=>{
+            this.setState({nome:e.target.value, pessoas:this.state.pessoas})
+          }}/>
+          <button onClick={this.noClick.bind(this)}>Enviar</button>
+          <ul>
+            {liPessoas}
+          </ul>
         </header>
       </div>
     );
   }
 }
 
-const firebaseAppAuth = firebaseApp.auth();
-
-const providers = {
-  googleProvider: new firebase.auth.GoogleAuthProvider(),
-};
-
-export default withFirebaseAuth({
-  providers,
-  firebaseAppAuth,
-})(App);
+export default App;
