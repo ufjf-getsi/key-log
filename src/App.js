5fba56be54498bb92a7f19bd6e43d5c265f8c2b3
Igor Knop <igorknop@gmail.com>
2019-04-12 01:42:11 -0300

    Adciona método para criar usuários

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
-import React, { Component } from 'react';
-import withFirebaseAuth from 'react-with-firebase-auth'
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import firebaseConfig from './firebaseConfig';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/firebase-firestore";
+import firebaseConfig from "./firebaseConfig";
+import logo from "./logo.svg";
+import "./App.css";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
+  cria() {
+    const { user, signOut, signInWithGoogle } = this.props;
+    firebaseDb
+      .collection("lab3")
+      .add({
+        portador: "Teste",
+        user: user.displayName,
+        datahora: new Date().getTime()
+      })
+      .then(ref => {
+        console.log(ref);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   render() {
-    const {
-      user,
-      signOut,
-      signInWithGoogle,
-    } = this.props;
+    const { user, signOut, signInWithGoogle } = this.props;
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {
-            user
-              ? <p>Hello, {user.displayName}</p>
-              : <p>Please sign in.</p>
-          }
+          {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
 
-          {
-            user
-              ? <button onClick={signOut}>Sign out</button>
-              : <button onClick={signInWithGoogle}>Sign in with Google</button>
-          }
+          {user ? (
+            <div>
+              <button onClick={signOut}>Sign out</button>
+              <button onClick={this.cria.bind(this)}>Cria</button>
+            </div>
+          ) : (
+            <button onClick={signInWithGoogle}>Sign in with Google</button>
+          )}
         </header>
       </div>
     );
@@ -38,12 +51,13 @@ class App extends Component {
 }
 
 const firebaseAppAuth = firebaseApp.auth();
+const firebaseDb = firebaseApp.firestore();
 
 const providers = {
-  googleProvider: new firebase.auth.GoogleAuthProvider(),
+  googleProvider: new firebase.auth.GoogleAuthProvider()
 };
 
 export default withFirebaseAuth({
   providers,
-  firebaseAppAuth,
+  firebaseAppAuth
 })(App);
--- src/firebaseConfig-dist.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
-};
-//Pegue os dados no console do firebase
