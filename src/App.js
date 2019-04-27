d8825e9541675da5c44e0ec9bb07bfbc090e086b
unknown <josuedelgadoheringer98@gmail.com>
2019-04-27 20:49:31 -0300

    arrumando problema com relacao a apagar a informação apos o envio

--- src/App.js
+++ src/App.js
@@ -79,45 +79,44 @@ class App extends Component {
         <option value={n} />
       );
     });
-      return (
-        <div className="App">
-          <header className="App-header">
-            <datalist id="nomes">{datalist}</datalist>
-            <img src={logo} className="App-logo" alt="logo" />
-            <Button id="login" variant="outlined"  onClick={signOut}>Sign out</Button>
-            {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
+    return (
+      <div className="App">
+        <header className="App-header">
+          <datalist id="nomes">{datalist}</datalist>
+          <img src={logo} className="App-logo" alt="logo" />
+          <Button id="login" variant="outlined" onClick={signOut}>Sign out</Button>
+          {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
+          {user ? (
+            <div>
+              <TextField id="myInput" list="nomes" label="Nome" value={this.state.nome} onChange={(e) => {
+                this.setState({ ...this.state, nome: e.target.value })
+              }} />
+              <Button variant="outlined" onClick={this.cria.bind(this)}>Enviar</Button>
+              <ul>{regs}</ul>
+            </div>
+          ) : (
+              <Button variant="outlined" onClick={signInWithGoogle}>Sign in with Google</Button>
+            )}
 
-            {user ? (
-              <div>
-                <TextField id="myInput" label="Nome" value={this.state.name} onChange={(e) => {
-                  this.setState({ ...this.state, nome: e.target.value })
-                }} />
-                <Button variant="outlined" onClick={this.cria.bind(this)}>Enviar</Button>
-                <ul>{regs}</ul>
-              </div>
-            ) : (
-                <Button variant="outlined"  onClick={signInWithGoogle}>Sign in with Google</Button>
-              )}
+        </header>
 
-          </header>
-
-          <footer>Desenvolvido por <a target="_blank" href="https://www.ufjf.br/getsi/">GET SI</a></footer>
-        </div>
-      );
-    }
+        <footer>Desenvolvido por <a target="_blank" href="https://www.ufjf.br/getsi/">GET SI</a></footer>
+      </div>
+    );
   }
+}
 
-  firebaseApp.initializeApp(firebaseConfig);
-  const firebaseAppAuth = firebaseApp.auth();
-  firebaseApp.firestore().enablePersistence();
-  const firebaseDb = firebaseApp.firestore();
+firebaseApp.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+firebaseApp.firestore().enablePersistence();
+const firebaseDb = firebaseApp.firestore();
 
-  const providers = {
-    googleProvider: new firebaseApp.auth.GoogleAuthProvider()
-  };
+const providers = {
+  googleProvider: new firebaseApp.auth.GoogleAuthProvider()
+};
 
 
-  export default withFirebaseAuth({
-    providers,
-    firebaseAppAuth
-  })(App);
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth
+})(App);
