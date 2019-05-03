acda1bc46f33c285b456c823f97fde122a56dba7
Igor Knop <igorknop@gmail.com>
2019-05-03 10:58:52 -0300

    Updates

---
+++ precache-manifest.de1b21e481dc3e12a99b1fea9ee741be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "88b281f1863d66c66117",
+    "url": "/static/js/main.d63ecdc7.chunk.js"
+  },
+  {
+    "revision": "3f0212e555e323813a1e",
+    "url": "/static/js/2.140cf8c0.chunk.js"
+  },
+  {
+    "revision": "88b281f1863d66c66117",
+    "url": "/static/css/main.82168d52.chunk.css"
+  },
+  {
+    "revision": "3b92fcddcd4361129330faa6e6740a41",
+    "url": "/index.html"
+  }
+];
